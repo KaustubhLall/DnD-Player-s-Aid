e381174561a01bb833237452c6b535baacd8a572
Kaustubh Lall <klall@ucsd.edu>
2017-03-28 13:28:20 -0700

    Working on the interactive shell for players.

--- src/player.py
+++ src/player.py
@@ -386,58 +386,6 @@ class Player:
         return d
 
 
-class PlayerInterface:
-    """This class serves as an interface between the player and the program."""
-
-    def out(self, message):
-        """
-        Sends a message to the player.
-        :param message: message to send.
-        :return: None.
-        """
-        print(message)
-
-    def read(self, message):
-        """
-        Prompts the user for input with the given message and returns the
-        string as lowercase.
-        :param message: prompt message.
-        :return: Player input as lowercase.
-        """
-        userResponse = input(message)
-
-        if len(userResponse) == 0:
-            userResponse = input(message)
-
-        return userResponse.lower()
-
-    def readRaw(self, message):
-        """
-        Prompts the user for input with the given message and returns the
-        string as-is.
-        :param message: prompt message.
-        :return: Player input.
-        """
-        userResponse = input(message)
-        if len(userResponse) == 0:
-            userResponse = input(message)
-        return userResponse
-
-    def next(self, message="", ch=" "):
-        """
-        Enters the next interaction with the user.
-        :param message: optional message to display.
-        :return: None
-        """
-        os.system('cls' if os.name == 'nt' else 'clear')
-        if message != "":
-            message = "+" + message + "+"
-            padding = CONST.TERMINAL_LENGTH - len(message)
-            padding /= 2
-            padding = int(padding)
-            out = message + (padding * ch)
-            print(out + "\n")
-
 
 class Item:
     """Defines an item object. The item typically will have a name, value,
--- src/shell.py
+++ src/shell.py
@@ -1,2 +1,30 @@
 # this script will run the main shell that interacts with the user.
-#
+# by default, the shell runs in this state.
+# however, subshells may be called by this shell.
+# for example, when a  player enters combat, the control is transferred to the
+# combat subshell. These will be defined as methods inside this package,
+# or within other .py files.
+shell =
+def combatSubshell(player):
+    """
+    Runs the combat subshell.
+    :param player: data of the current player as a player object.
+    :return:
+    """
+
+
+def inventorySubshell(player):
+    """
+    Does inventory interactions with the player. Money-interactions will
+    likely jsut be implemented into the main subshell.
+    :param player: player whose data is to be manipulated.
+    :return:
+    """
+
+def helpSubShell(player):
+    """
+    Experimental subshell that might be implemented to have useful context/help
+    for items.
+    :param player: probably not relevant
+    :return:
+    """
